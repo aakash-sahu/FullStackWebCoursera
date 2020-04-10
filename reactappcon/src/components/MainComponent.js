5ed307274514076a17455ff48a7ca4108033754c
coolakss <coolakss@yahoo.com>
2020-04-09 23:44:38 -0400

    single page application part 1 with updated home page

--- reactappcon/src/components/MainComponent.js
+++ reactappcon/src/components/MainComponent.js
@@ -3,12 +3,16 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import DishDetails from './DishdetailComponent'
-import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect} from 'react-router-dom'
 import { Container } from 'reactstrap';
+import Contact from './ContactComponent';
+import { DISHES} from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS} from '../shared/promotions';
 
 class Main extends Component {
   
@@ -16,6 +20,9 @@ class Main extends Component {
     super(props);
     this.state= {
       dishes: DISHES,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
+      comments: COMMENTS,
     };
   }
 
@@ -23,7 +30,10 @@ class Main extends Component {
     
     const HomePage = () => {
       return (
-        <Home />
+        <Home dish={this.state.dishes.filter((dish)=> dish.featured)[0]} 
+        promotion={this.state.promotions.filter((promo)=> promo.featured)[0]}
+        leader={this.state.leaders.filter((leader)=> leader.featured)[0]}
+        />
       )
     }
 
@@ -33,6 +43,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path="/contactus" component= {Contact} />
           <Redirect to="/home" />
         </Switch>
         {/* <Menu dishes={this.state.dishes} onClick = {(dishID)=>this.onDishSelect(dishID)}/>
