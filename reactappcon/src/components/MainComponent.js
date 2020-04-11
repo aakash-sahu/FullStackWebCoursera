df4b844a0187ea45cf6111a350eaf2fddad99c8b
coolakss <coolakss@yahoo.com>
2020-04-11 16:22:14 -0400

    Assignment 2 About us page

--- reactappcon/src/components/MainComponent.js
+++ reactappcon/src/components/MainComponent.js
@@ -2,17 +2,17 @@
 // Setting this as Container component
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import DishDetails from './DishdetailComponent'
+import DishDetails from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect} from 'react-router-dom'
-import { Container } from 'reactstrap';
 import Contact from './ContactComponent';
 import { DISHES} from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS} from '../shared/promotions';
+import About from './AboutComponent';
 
 class Main extends Component {
   
@@ -38,8 +38,6 @@ class Main extends Component {
     }
 
     const DishWithId = ({match}) => {
-      console.log("Entered dishwithid func")
-      console.log(this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0])
       return (
         <DishDetails selectedDish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
           comments= {this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
@@ -55,6 +53,7 @@ class Main extends Component {
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component= {Contact} />
+          <Route path="/aboutus" component= {() => <About leaders = {this.state.leaders} />} />
           <Redirect to="/home" />
         </Switch>
         {/* <Menu dishes={this.state.dishes} onClick = {(dishID)=>this.onDishSelect(dishID)}/>
