9a98dd2873a4fc2acb45db00fc4b37f012cde8b0
coolakss <coolakss@yahoo.com>
2020-05-05 23:42:55 -0400

    Express Router for REST API

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = '3000';
 
@@ -10,51 +11,29 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-//rest api endpoints --order matters
-// /dishes
-app.all('/dishes', (req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
-
-app.get('/dishes', (req,res,next) => {
-    res.end("Will send all the dishes to you")
-});
-
-app.post('/dishes', (req,res,next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details '+ req.body.description);
-});
-
-app.put('/dishes', (req,res,next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes');
-});
-
-app.delete('/dishes', (req,res,next) => {
-    res.end("Deleting all the dishes");
-});
+//mount the router at end-point
+app.use('/dishes', dishRouter);
 
 // /dishes/dishid
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end("Will send details of the dish: " + req.params.dishId +' to you.')
-});
+// app.get('/dishes/:dishId', (req,res,next) => {
+//     res.end("Will send details of the dish: " + req.params.dishId +' to you.')
+// });
 
-app.post('/dishes/:dishId', (req,res,next) => {
-    res.statusCode = 403;
-    res.end('Post operation not supported on /dishes'+ req.params.dishId);
-});
+// app.post('/dishes/:dishId', (req,res,next) => {
+//     res.statusCode = 403;
+//     res.end('Post operation not supported on /dishes'+ req.params.dishId);
+// });
 
-app.put('/dishes/:dishId', (req,res,next) => {
-    res.write('Updating the dish: '+req.params.dishId + '\n');
-    res.end('Will update the dish: '+ req.body.name + ' with details'+ req.body.description);
-});
+// app.put('/dishes/:dishId', (req,res,next) => {
+//     res.write('Updating the dish: '+req.params.dishId + '\n');
+//     res.end('Will update the dish: '+ req.body.name + ' with details'+ req.body.description);
+// });
 
-app.delete('/dishes/:dishId', (req,rest,next) => {
-    res.end("Deleting the dish: " + req.params.dishId);
-});
+// app.delete('/dishes/:dishId', (req,rest,next) => {
+//     res.end("Deleting the dish: " + req.params.dishId);
+// });
 
-//server static files --order matter
+//server static files --order matter- put after API
 app.use(express.static(__dirname+ '/public'));
 
 app.use((req, res, next) => {
---
+++ NodeJS/packages.md
@@ -0,0 +1,5 @@
+Packages to install in the the serverside 
+1. NodeJS
+2. npm install express$4.16.3 --save
+3. npm install morgan@1.9.0 --save (for logging)
+4. npm install body-parser@1.18.3 --save
