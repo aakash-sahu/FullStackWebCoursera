c3d28f6249daef0f35abb8018b6a8b8a365f2be5
coolakss <coolakss@yahoo.com>
2020-05-06 22:39:12 -0400

    Assignment 1- Routes for dishes, leaders, and promos along with id param

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -4,6 +4,9 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
 const hostname = 'localhost';
 const port = '3000';
 
@@ -13,25 +16,8 @@ app.use(bodyParser.json());
 
 //mount the router at end-point
 app.use('/dishes', dishRouter);
-
-// /dishes/dishid
-// app.get('/dishes/:dishId', (req,res,next) => {
-//     res.end("Will send details of the dish: " + req.params.dishId +' to you.')
-// });
-
-// app.post('/dishes/:dishId', (req,res,next) => {
-//     res.statusCode = 403;
-//     res.end('Post operation not supported on /dishes'+ req.params.dishId);
-// });
-
-// app.put('/dishes/:dishId', (req,res,next) => {
-//     res.write('Updating the dish: '+req.params.dishId + '\n');
-//     res.end('Will update the dish: '+ req.body.name + ' with details'+ req.body.description);
-// });
-
-// app.delete('/dishes/:dishId', (req,rest,next) => {
-//     res.end("Deleting the dish: " + req.params.dishId);
-// });
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 //server static files --order matter- put after API
 app.use(express.static(__dirname+ '/public'));
