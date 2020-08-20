e9479f031b684d56631d9229a04856d1e1f8793a
coolakss <coolakss@yahoo.com>
2020-08-19 22:13:14 -0400

    assignment 1-added about and contact component along with drawer and stack navigation

--- reactnativecon/components/MainComponent.js
+++ reactnativecon/components/MainComponent.js
@@ -7,6 +7,8 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 const MenuNavigator = createStackNavigator(); 
 
@@ -60,13 +62,68 @@ function HomeNavigatorScreen({ navigation }) {
     );
 }
 
+// Stack navigator for Contact Component
+const ContactNavigator = createStackNavigator();
+
+function ContactNavigatorScreen({ navigation }) {
+    return (
+        <ContactNavigator.Navigator
+        initialRouteName='Contact'
+        screenOptions={{
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+                color: "#fff"            
+            }
+        }}
+    >
+        <ContactNavigator.Screen
+            name="Contact"
+            component={Contact}
+        />         
+    </ContactNavigator.Navigator>
+    );
+}
+
+// Stack navigator for Contact Component
+const AboutNavigator = createStackNavigator();
+
+function AboutNavigatorScreen({ navigation }) {
+    return (
+        <AboutNavigator.Navigator
+        initialRouteName='About Us'
+        screenOptions={{
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+                color: "#fff"            
+            }
+        }}
+    >
+        <AboutNavigator.Screen
+            name="About Us"
+            component={About}
+        />         
+    </AboutNavigator.Navigator>
+    );
+}
+
+//Drawer based Navigation
 const Drawer = createDrawerNavigator();
 
 function MainNavigator({ navigation }) {
     return(
-        <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Navigator 
+            initialRouteName="Home"
+            drawerStyle ={{backgroundColor: '#D1C4E9'}}>
             <Drawer.Screen name="Home" component={HomeNavigatorScreen} />
             <Drawer.Screen name="Menu" component={MenuNavigatorScreen} />
+            <Drawer.Screen name="Contact" component={ContactNavigatorScreen} />
+            <Drawer.Screen name="About" component={AboutNavigatorScreen} />
         </Drawer.Navigator>
     );
 }
